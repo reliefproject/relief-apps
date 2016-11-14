5627927c578b1fc118754b9045d1421dff4922c9
toenu23 <fubarphukhew@gmail.com>
2016-11-14 09:58:20 +0100

    Improved search results

--- js/services/packages.js
+++ js/services/packages.js
@@ -29,21 +29,43 @@
 
 
       getSearchResults: function(query) {
+        let aliasData;
         return Relief.nxt.request({
-          requestType: 'searchTaggedData',
-          query: query,
-          // channel: '',
-          includeData: true,
+          requestType: 'getAlias',
+          aliasName: query
+        })
+        .then(function(result) {
+          if (result.data.aliasURI) {
+            aliasData = result.data.aliasURI;
+          }
+          return Relief.nxt.request({
+            requestType: 'searchTaggedData',
+            query: query,
+            // channel: '',
+            includeData: true,
+          });
         })
         .then(function(result) {
           return new Promise(function(resolve, reject) {
             if (!result.data.data) {
               return reject(result.data.err);
             }
-            resolve(result.data.data);
+            let returnVal = {
+              query: query,
+              package: {},
+              similarPackages: [],
+            };
+            for (let i in result.data.data) {
+              let data = result.data.data[i];
+              if (data.transaction === aliasData) {
+                returnVal.package = data;
+                continue;
+              }
+              returnVal.similarPackages.push(data);
+            }
+            resolve(returnVal);
           });
         });
-        // TODO check if alias === name
       },
 
 
